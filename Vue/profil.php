8e7c7841f1761855593c2d2b8f5348c07ded7b0b
MegaProjet <hardy.k@neuf.fr>
2015-04-09 11:56:00 +0200

    Ajout de l'adresse dans profil + validerprofil

--- Vue/profil.php
+++ Vue/profil.php
@@ -11,88 +11,106 @@
     $query->execute(array($_SESSION['id']));
 
     while ($row = $query->fetch()) {
-        ?>  
-    <input type="hidden" id="id_util" value="<?php echo $_SESSION['id'] ?>" />
-        <div class="row">
-            <div class="col-lg-1">
-                <label for="nom">Nom:</label>
-            </div>
-            <div class="col-lg-2">
-                <input type="text" id="nom" value="<?php echo $row['nom'] ?>" />
-            </div>
+        $query = $bdd->prepare('SELECT `numero`, `code_postal`, `rue`, `ville`, `latitude`, `longitude` FROM `adresse` WHERE `id_adr`= ?');
+        $query->execute(array($row['id_adresse']));
+        while ($row2 = $query->fetch()) {
+            ?>
+            <input type="hidden" id="id_util" value="<?php echo $_SESSION['id'] ?>" />
+             <input type="hidden" id="id_adr" value="<?php echo $row['id_adresse'] ?>" />
+            <div class="row">
+                <div class="col-lg-1">
+                    <label for="nom">Nom:</label>
+                </div>
+                <div class="col-lg-2">
+                    <input type="text" id="nom" value="<?php echo $row['nom'] ?>" />
+                </div>
 
 
-        </div>
-        <div class="row">
-            <div class="col-lg-1">
-                <label for="prenom">Prenom:</label>
             </div>
-            <div class="col-lg-2">
-                <input type="text" id="prenom" value="<?php echo $row['prenom'] ?>" />
+            <div class="row">
+                <div class="col-lg-1">
+                    <label for="prenom">Prenom:</label>
+                </div>
+                <div class="col-lg-2">
+                    <input type="text" id="prenom" value="<?php echo $row['prenom'] ?>" />
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col-lg-1">
+                    <label for="nom_artiste">Nom d'artiste:</label>
+                </div>
+                <div class="col-lg-2">
+                    <input type="text" id="nom_artiste" value="<?php echo $row['nom_artiste'] ?>"/>
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col-lg-1">
+                    <label for="age">Age:</label>
+                </div>
+                <div class="col-lg-2">
+                    <input type="text" id="age" value="<?php echo $row['age'] ?>"/>
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col-lg-1">
+                    <label for="mail">Mail:</label>
+                </div>
+                <div class="col-lg-2">
+                    <input type="text" id="mail" value="<?php echo $row['mail'] ?>"/>  
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col-lg-1">
+                    <label for="telephone">Telephone:</label>
+                </div>
+                <div class="col-lg-2">
+                    <input type="text" id="telephone" value="<?php echo $row['telephone'] ?>"/>
+                </div>
+            </div>
+            <div class="row">
+
+                <div class="col-lg-1">
+                    <label for = "adresse">Adresse: </label>
+                </div>
+                <div class="col-lg-4 " id= "numerobouton"> 
+                    <input name="numero" id="numero" type="text" step="1" placeholder="n°" value="<?php echo $row2['numero'] ?> " style="width: 40px;"/>                             
+                    <input name="adresse" id="adresse" type="text" placeholder="rue" value="<?php echo $row2['rue'] ?> " style="width: 140px;">
+                    <input name="CP" id="CP" type="text" placeholder="CP" value="<?php echo $row2['code_postal'] ?>" style="width: 140px;">
+                    <input name="ville" id="ville" type="text" placeholder="ville   " value="<?php echo $row2['ville'] ?>" style="width: 140px;">
+                </div>
+
+            </div>
+            <div class="row">
+
+                <div class="col-lg-1">
+                    <label for="password">Mot de passe:</label> 
+                </div>
+                <div class="col-lg-2">
+                    <input id ="password" type="password">
+                </div>
+            </div>
+            <div class="row">
+
+                <div class="col-lg-1">
+                    <label for="password2">Confirmer:</label>
+                </div>
+                <div class="col-lg-2">
+                    <input id="password2" type="password"> 
+                </div>
             </div>
 
-        </div>
-        <div class="row">
-            <div class="col-lg-1">
-                <label for="nom_artiste">Nom d'artiste:</label>
-            </div>
-            <div class="col-lg-2">
-                <input type="text" id="nom_artiste" value="<?php echo $row['nom_artiste'] ?>"/>
-            </div>
+            <h3>Description</h3>
+            <hr>
 
-        </div>
-        <div class="row">
-            <div class="col-lg-1">
-                <label for="age">Age:</label>
-            </div>
-            <div class="col-lg-2">
-                <input type="text" id="age" value="<?php echo $row['age'] ?>"/>
-            </div>
-
-        </div>
-        <div class="row">
-            <div class="col-lg-1">
-                <label for="mail">Mail:</label>
-            </div>
-            <div class="col-lg-2">
-                <input type="text" id="mail" value="<?php echo $row['mail'] ?>"/>  
-            </div>
-
-        </div>
-        <div class="row">
-            <div class="col-lg-1">
-                <label for="telephone">Telephone:</label>
-            </div>
-            <div class="col-lg-2">
-                <input type="text" id="telephone" value="<?php echo $row['telephone'] ?>"/>
-            </div>
-        </div>
-        <div class="row">
-
-            <div class="col-lg-1">
-                Mot de passe:  
-            </div>
-            <div class="col-lg-2">
-                <input id ="password" type="password">
-            </div>
-        </div>
-        <div class="row">
-
-            <div class="col-lg-1">
-                Confirmer:
-            </div>
-            <div class="col-lg-2">
-                <input id="password2" type="password"> 
-            </div>
-        </div>
-
-        <h3>Description</h3>
-        <hr>
-
-        <textarea id="description"> <?php echo $row['descr_util'] ?></textarea>
+            <textarea id="description"> <?php echo $row['descr_util'] ?></textarea>
         <?php
     }
-    ?>
+}
+?>
     <br/><br/><br/>
     <div class="row">
         <div col-lg-1>
@@ -123,13 +141,18 @@
             var password2 = $("#password2").val();
             var description = $("#description").val();
             var id_util = $("#id_util").val();
+            var id_adr = $("#id_adr").val();
+            var numero = $("#numero").val();
+            var adresse = $("#adresse").val();
+            var CP = $("#CP").val();
+            var ville = $("#ville").val();
 
 
 
             $.ajax({
                 method: "GET",
                 url: "../php/validerprofil.php",
-                data: {id_util : id_util,nom: nom, prenom: prenom, nom_artiste: nom_artiste, age: age, telephone: telephone, mail: mail, password: password, password2: password2, description: description},
+                data: {id_util: id_util, nom: nom, prenom: prenom, nom_artiste: nom_artiste, age: age, telephone: telephone, mail: mail, password: password, password2: password2, description: description, numero: numero,adresse:adresse,CP:CP,ville:ville,id_adr : id_adr},
                 dataType: 'html',
                 success: function (data) {
 
--- php/validerprofil.php
+++ php/validerprofil.php
@@ -12,30 +12,44 @@ $description = $_GET['description'];
 $age = $_GET['age'];
 $nom_artiste = $_GET['nom_artiste'];
 $id_util = $_GET['id_util'];
-echo "testttt" . $id_util;
+$id_adr = $_GET['id_adr'];
+$numero = $_GET['numero'];
+$adresse = $_GET['adresse'];
+$CP = $_GET['CP'];
+$ville = $_GET['ville'];
 
-if ($password != "" && $password2 != "" && $nom != "" && $prenom != "" && $mail != "" && $telephone != "" && $description != "" && $age != "" && $nom_artiste != "") {
-    if ($password == $password2) {
-        
-        
-        
-        
-        
-        
-     $query=$bdd->prepare('UPDATE `utilisateur` SET `password`=?,`nom`=?,`prenom`=?,`nom_artiste`=?,`age`=?,`mail`=?,`telephone`=?,`descr_util`=? WHERE `id_util`=?');
+if ($nom != "" && $prenom != "" && $mail != "" && $telephone != "" && $description != "" && $age != "" && $nom_artiste != "" && $numero != "" && $adresse != "" && $CP != "" && $ville != "") {
+
+    if ($password != "" && $password2 != "" && $password == $password2) {
+
+
+
+
+
+
+        $query = $bdd->prepare('UPDATE `utilisateur` SET `password`=?,`nom`=?,`prenom`=?,`nom_artiste`=?,`age`=?,`mail`=?,`telephone`=?,`descr_util`=? WHERE `id_util`=?');
+        $query2 = $bdd->prepare('UPDATE adresse SET numero = ? , rue = ? , code_postal = ? , ville = ? WHERE id_adr = ?');
         try {
-               $query->execute(array($password,$nom,$prenom,$nom_artiste,$age,$mail,$telephone,$description,$id_util));
-                echo "UPDATE ok";
+            $query->execute(array($password, $nom, $prenom, $nom_artiste, $age, $mail, $telephone, $description, $id_util));
+            $query2->execute(array($numero,$adresse,$CP,$ville,$id_adr));
+            echo "UPDATE ok";
         } catch (Exception $ex) {
             echo 'UPDATE fail';
         }
-     
-        
-        
-    }else {
+    } else if ($password == "" && $password2 == "") {
+
+        $query = $bdd->prepare('UPDATE `utilisateur` SET `nom`=?,`prenom`=?,`nom_artiste`=?,`age`=?,`mail`=?,`telephone`=?,`descr_util`=? WHERE `id_util`=?');
+        $query2 = $bdd->prepare('UPDATE adresse SET numero = ? , rue = ? , code_postal = ? , ville = ? WHERE id_adr = ?');
+        try {
+            $query->execute(array($nom, $prenom, $nom_artiste, $age, $mail, $telephone, $description, $id_util));
+            $query2->execute(array($numero,$adresse,$CP,$ville,$id_adr));
+            echo "UPDATE ok";
+        } catch (Exception $ex) {
+            echo 'UPDATE fail';
+        }
+    } else {
         echo 'Password != password2';
     }
-    
 } else {
     echo "Un des champs est vide ! ";
 }
