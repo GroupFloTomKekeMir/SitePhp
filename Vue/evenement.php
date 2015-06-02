8f43e695b1eea6fb26fc9979c336f5abfe7ec175
flo <floriansamson666@gmail.com>
2015-06-02 09:42:34 +0200

    9:42

--- Vue/evenement.php
+++ Vue/evenement.php
@@ -3,14 +3,14 @@
 <?php include('../php/connexionBdd.php'); ?>
 <html>
     <head>
-        <title>Simple Map</title>
+        <title>Evènements</title>
         <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
         <meta charset="utf-8">
         <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
     </head>
     <body>
         <!-- <div clas="row">-->
-        <div class="col-lg-6">
+        <div class="col-lg-offset-1 col-lg-5">
 
             <?php
             $query = $bdd->prepare('SELECT `id_evenement`, `nom`, `description`, `date`, `id_adresse`, `id_annonceur` FROM `evenement`');
@@ -20,13 +20,21 @@
                 ?>
 
 
-                <h6><?php echo $row['nom'] ?> </h6>
+                <h4><?php echo $row['nom'] ?> </h4>
                 <h6><?php echo $row['description'] ?> </h6>
                 <h6><?php echo $row['date'] ?> </h6>
-                <button class="eve" value="<?php echo $row['id_evenement'] ?>">Voir</button>
-                <p>---------------------------------</p>
 
+                <div class="row">
+                    <div class="col-lg-offset-4 col-lg-4 ">
+                        <button class="eve" value="<?php echo $row['id_evenement'] ?>">Voir</button>
+                    </div>
+                </div> 
 
+                <div class="row">
+                    <div class="col-lg-offset-1 col-lg-8 ">
+                        <div class="traitEvenement"></div>
+                    </div>
+                </div>        
                 <?php
             }
             ?>
@@ -34,7 +42,7 @@
         </div>
 
 
-        <div class="col-lg-6" id="map-canvas"></div>
+        <div class="col-lg-5" id="map-canvas"></div>
         <!-- </div>-->
     </body>
 </html>
@@ -51,8 +59,7 @@
             map = new google.maps.Map(document.getElementById('map-canvas'),
                     mapOptions);
         }
-
-
+        
         google.maps.event.addDomListener(window, 'load', initialize);
         $(".eve").click(function () {
 
@@ -64,38 +71,25 @@
                 data: {query: valEvenement},
                 dataType: 'html',
                 success: function (data) {
-                    
-                    if(!marker) { 
+
+                    if (!marker) {
                     } else {
                         marker.setMap(null);
                     }
-                      
-                   
-                 
+
                     var lat = data.substring(0, data.indexOf(' '));
                     var long = data.substring(data.lastIndexOf(' ') + 1);
 
-                    var myLatlng = new google.maps.LatLng(lat,long);
+                    var myLatlng = new google.maps.LatLng(lat, long);
 
                     marker = new google.maps.Marker({
                         position: myLatlng,
                         map: map,
                         title: 'Hello World!'
                     });
-
-
                     //alert(data);
                 }
             })
-
-
-
-
-
-
-
-
-
         });
     });
 </script>  
--- Vue/index.php
+++ Vue/index.php
@@ -1,199 +1,199 @@
-<?php  include('header.php'); ?>
+<?php include('header.php'); ?>
 
- <?php var_dump($_SESSION['id']) ?>
- <style type="text/css">
-            /* bootstrap 3 helpers */
+<?php // var_dump($_SESSION['id']) ?>
+<style type="text/css">
+    /* bootstrap 3 helpers */
 
-.navbar-form input, .form-inline input {
-	width:auto;
-}
+    .navbar-form input, .form-inline input {
+        width:auto;
+    }
 
-/* end */
+    /* end */
 
-/* custom theme */
+    /* custom theme */
 
-header {
-	min-height:230px;
-  	margin-bottom:5px;
-}
+    header {
+        min-height:230px;
+        margin-bottom:5px;
+    }
 
-@media (min-width: 979px) {
-  #sidebar.affix-top {
-      position: static;
-  }
-  
-  #sidebar.affix {
-      position: fixed;
-      top: 0;
-      width:21.2%;
-  }
-}
+    @media (min-width: 979px) {
+        #sidebar.affix-top {
+            position: static;
+        }
 
-.affix,.affix-top {
-   position:static;
-}
+        #sidebar.affix {
+            position: fixed;
+            top: 0;
+            width:21.2%;
+        }
+    }
 
-/* theme */
+    .affix,.affix-top {
+        position:static;
+    }
 
-body {
-  color:#828282;
-  background-color:#eee;
-}
+    /* theme */
 
-a,a:hover {
-	color:#ff3333;
-  	text-decoration:none;
-}
+    body {
+        color:#828282;
+        background-color:#eee;
+    }
 
-.highlight-bk {
-	background-color:#ff3333;
-    padding:1px;
-    width:100%;
-}
+    a,a:hover {
+        color:#ff3333;
+        text-decoration:none;
+    }
 
-.highlight {
-	color:#ff3333;
-}
-  
-h3.highlight  {
-	padding-top:13px;
-    padding-bottom:14px;
-  	border-bottom:2px solid #ff3333;
-}
+    .highlight-bk {
+        background-color:#ff3333;
+        padding:1px;
+        width:100%;
+    }
 
-.navbar {
-	background-color:#ff3333;
-  	color:#ffffff;
-    border:0;
-  	border-radius:0;
-}
-.navbar-nav > li > a {
-  	color:#fff;
-  	padding-left:20px;
-  	padding-right:20px;
-  	border-left:1px solid #ee3333;
-}
-.navbar-nav > li > a:hover, .navbar-nav > li > a:focus {
-    color:#666666; 
-}
-.navbar-nav > li:last-child > a {
-  	border-right:1px solid #ee3333;
-}
-.navbar-nav > .active > a, .navbar-nav > .active > a:hover, .navbar-nav > .active > a:focus {
-    color: #ffffff;
-	background-color:transparent;
-}
-.navbar-nav > .open > a, .navbar-nav > .open > a:hover, .navbar-nav > .open > a:focus {
-	color: #f0f0f0;
-	background-color:transparent;
-  	opacity:.9;
-    border-color:#ff3333;
-}
+    .highlight {
+        color:#ff3333;
+    }
 
-.nav .open > a {
-    border-color:#777777;
-    border-width:0;
-}
+    h3.highlight  {
+        padding-top:13px;
+        padding-bottom:14px;
+        border-bottom:2px solid #ff3333;
+    }
 
-.accordion-group {
-	border-width:0;
-}
+    .navbar {
+        background-color:#ff3333;
+        color:#ffffff;
+        border:0;
+        border-radius:0;
+    }
+    .navbar-nav > li > a {
+        color:#fff;
+        padding-left:20px;
+        padding-right:20px;
+        border-left:1px solid #ee3333;
+    }
+    .navbar-nav > li > a:hover, .navbar-nav > li > a:focus {
+        color:#666666; 
+    }
+    .navbar-nav > li:last-child > a {
+        border-right:1px solid #ee3333;
+    }
+    .navbar-nav > .active > a, .navbar-nav > .active > a:hover, .navbar-nav > .active > a:focus {
+        color: #ffffff;
+        background-color:transparent;
+    }
+    .navbar-nav > .open > a, .navbar-nav > .open > a:hover, .navbar-nav > .open > a:focus {
+        color: #f0f0f0;
+        background-color:transparent;
+        opacity:.9;
+        border-color:#ff3333;
+    }
 
-.dropdown-menu {
-	min-width: 250px;
-}
+    .nav .open > a {
+        border-color:#777777;
+        border-width:0;
+    }
 
-.accordion-heading .accordion-toggle, .accordion-inner, .nav-stacked li > a {
-	padding-left:1px;
-}
+    .accordion-group {
+        border-width:0;
+    }
 
-.caret {
-	color:#fff;
-}
+    .dropdown-menu {
+        min-width: 250px;
+    }
 
-.navbar-toggle {
-	color:#fff;
-    border-width:0;
-}
-  
-.navbar-toggle:hover {
-	background-color:#fff;
-}
+    .accordion-heading .accordion-toggle, .accordion-inner, .nav-stacked li > a {
+        padding-left:1px;
+    }
 
-.panel {
-	padding-left:27px;
-    padding-right:27px;
-}
+    .caret {
+        color:#fff;
+    }
 
-/* end theme */
-        </style>
+    .navbar-toggle {
+        color:#fff;
+        border-width:0;
+    }
 
-        <header class="masthead">
-  <div class="container">
-    <div class="row">
-      <div class="col col-sm-6">
-        <h1><a href="#" title="scroll down for your viewing pleasure">MegaCasting</a>
-          <p class="lead">Le leader dans le recrutement d'artistes</p></h1>
-      </div>
-      <div class="col col-sm-6">
-        <div class="well pull-right">
-          <img src="//placehold.it/280x100/E7E7E7">        
+    .navbar-toggle:hover {
+        background-color:#fff;
+    }
+
+    .panel {
+        padding-left:27px;
+        padding-right:27px;
+    }
+
+    /* end theme */
+</style>
+
+<header class="masthead">
+    <div class="container">
+        <div class="row">
+            <div class="col col-sm-6">
+                <h1><a href="#" title="scroll down for your viewing pleasure">MegaCasting</a>
+                    <p class="lead">Le leader dans le recrutement d'artistes</p></h1>
+            </div>
+            <div class="col col-sm-6">
+                <div class="well pull-right">
+                    <img src="//placehold.it/280x100/E7E7E7">        
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-  
-  <div class="container">
-	<div class="row">
-      <div class="col col-sm-12">
-        
-        <div class="panel">
-        <div class="panel-body">
-         MegaCasting vous permet de découvrir de nombreuses offres et de vous forger un vrai parcours profesionnel.
+
+    <div class="container">
+        <div class="row">
+            <div class="col col-sm-12">
+
+                <div class="panel">
+                    <div class="panel-body">
+                        MegaCasting vous permet de découvrir de nombreuses offres et de vous forger un vrai parcours profesionnel.
+                    </div>
+                </div>
+
+            </div>
         </div>
-        </div>
-        
-      </div>
-  	</div>
-  </div>
+    </div>
 </header>
 
 <!-- Begin Body -->
 <div class="container">
-	<div class="row">
-  			<div class="col col-sm-3">
-              	<div id="sidebar">
-      			<ul class="nav nav-stacked">
+    <div class="row">
+        <div class="col col-sm-3">
+            <div id="sidebar">
+                <ul class="nav nav-stacked">
                     <li><h3 class="highlight">A propos <i class="glyphicon glyphicon-dashboard pull-right"></i></h3></li>
-                  	<li><a href="#">Twitter</a></li>
-          			<li><a href="#">Facebook</a></li>
-				</ul>
-               </div>
-      		</div>  
-      		<div class="col col-sm-9">
-              <div class="panel">
-              <h1>Bienvenue</h1>
-              
-              <div class="row">
-              	<div class="col col-sm-8">
-                    <img src="./rideau2.jpg" class="img-responsive">
-                </div> 
-        		<div class="col col-sm-4">
-                  <img src="//placehold.it/400x180/FF3333/FFF" class="img-responsive">
-                  <h4>Aside</h4>
-                  <hr>
-                  <img src="//placehold.it/400x180/FF3333/FFF" class="img-responsive">
-                  <h4>Aside 2</h4>
-              	</div>   
-              </div>
-        
-              	<h2>Titre 1 </h2>
+                    <li><a href="#">Twitter</a></li>
+                    <li><a href="#">Facebook</a></li>
+                </ul>
+            </div>
+        </div>  
+        <div class="col col-sm-9">
+            <div class="panel">
+                <h1>Bienvenue</h1>
+
+                <div class="row">
+                    <div class="col col-sm-8">
+                        <img src="./rideau2.jpg" class="img-responsive">
+                    </div> 
+                    <div class="col col-sm-4">
+                        <img src="//placehold.it/400x180/FF3333/FFF" class="img-responsive">
+                        <h4>Aside</h4>
+                        <hr>
+                        <img src="//placehold.it/400x180/FF3333/FFF" class="img-responsive">
+                        <h4>Aside 2</h4>
+                    </div>   
+                </div>
+
+                <h2>Titre 1 </h2>
                 Totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
                 dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
                 eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
                 sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
                 Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-             	</div>
-      	</div> 
-  	</div>
+            </div>
+        </div> 
+    </div>
 </div>
